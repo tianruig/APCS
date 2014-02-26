65deb8f9fc5620e4613148ae6a33ed6e83dee0e8
Tianrui Guo <tianrui@pac-rom.com>
2014-02-25 19:13:15 -0800

    Import rest of code from netbeans

--- Unit09/src/Exercises.java
+++ Unit09/src/Exercises.java
@@ -9,20 +9,27 @@ public class Exercises {
         int p = 0;
         int s = 0;
 
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             char c = e.getRandomRps();
-            switch (c){
+            switch (c) {
                 case 'r':
-                r++;
-                break;
+                    r++;
+                    break;
             }
         }
     }
-    public char getRandomRps(){
-        char [] rps = new char[14];
-        for (int i = 0; i < 3; i++){ rps[i] = 'r'; };
-        for (int i = 3; i < 8; i++){ rps[i] = 'p'; };
-        for (int i = 8; i < 14; i++){ rps[i] = 's'; };
-        return rps[(int)(Math.random() * 14)];
+
+    public char getRandomRps() {
+        char[] rps = new char[14];
+        for (int i = 0; i < 3; i++) {
+            rps[i] = 'r';
+        }
+        for (int i = 3; i < 8; i++) {
+            rps[i] = 'p';
+        }
+        for (int i = 8; i < 14; i++) {
+            rps[i] = 's';
+        }
+        return rps[(int) (Math.random() * 14)];
     }
 }
