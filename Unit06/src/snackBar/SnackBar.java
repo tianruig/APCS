9facec45f9fe69ee27b53c96c7c52e81f1853445
Tianrui Guo <tianrui@pac-rom.com>
2014-03-02 19:48:02 -0800

    Unit06: SnackBar: Support any (reasonable) number of vending machines
    The number of machines can be set via a a command-line argument. Default is 3 machines.

--- Unit06/src/snackBar/SnackBar.java
+++ Unit06/src/snackBar/SnackBar.java
@@ -8,30 +8,48 @@ import java.awt.event.ActionListener;
 public class SnackBar extends JFrame
         implements ActionListener {
     private static final String MY_PASSWORD = "jinx";
-    private VendingMachine machine1, machine2, machine3;
+    //    private VendingMachine machine1, machine2, machine3;
+    private VendingMachine[] machines;
 
-    public SnackBar() {
+    public SnackBar(int numMachines) {
         super("Snack Bar");
 
-        Color brandColor1 = new Color(130, 30, 10); // r, g, b
-        Color brandColor2 = new Color(255, 180, 0);
-        Color brandColor3 = new Color(90, 180, 0);
+        Color[] brandColor = new Color[numMachines];
+
+        for (int i = 0; i < brandColor.length; i++) {
+            brandColor[i] = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+        }
+
+//        Color brandColor1 = new Color(130, 30, 10); // r, g, b
+//        Color brandColor2 = new Color(255, 180, 0);
+//        Color brandColor3 = new Color(90, 180, 0);
 
         // Load the coin icon for the vending machine buttons:
         ImageIcon coin = new ImageIcon("coin.gif");
 
-        machine1 = new VendingMachine("Java", brandColor1, 45, coin);
-        machine2 = new VendingMachine("JApple", brandColor2, 50, coin);
-        machine3 = new VendingMachine("Jinx", brandColor3, 35, coin);
+        machines = new VendingMachine[numMachines];
+
+        for (int i = 0; i < machines.length; i++) {
+            machines[i] = new VendingMachine("Product " + (i + 1), brandColor[i], (int) (Math.random() * 90 + 10), coin);
+        }
+
+//        machine1 = new VendingMachine("Java", brandColor1, 45, coin);
+//        machine2 = new VendingMachine("JApple", brandColor2, 50, coin);
+//        machine3 = new VendingMachine("Jinx", brandColor3, 35, coin);
 
         Box wall = Box.createHorizontalBox();
         wall.add(Box.createHorizontalStrut(5));
-        wall.add(machine1);
-        wall.add(Box.createHorizontalStrut(5));
-        wall.add(machine2);
-        wall.add(Box.createHorizontalStrut(5));
-        wall.add(machine3);
-        wall.add(Box.createHorizontalStrut(5));
+        for (int i = 0; i < machines.length; i++) {
+            wall.add(machines[i]);
+            wall.add(Box.createHorizontalStrut(5));
+        }
+
+//        wall.add(machine1);
+//        wall.add(Box.createHorizontalStrut(5));
+//        wall.add(machine2);
+//        wall.add(Box.createHorizontalStrut(5));
+//        wall.add(machine3);
+//        wall.add(Box.createHorizontalStrut(5));
 
         JPanel service = new JPanel();
         service.add(new JLabel(" Service login: "));
@@ -54,9 +72,12 @@ public class SnackBar extends JFrame
         password.setText("");
         if (MY_PASSWORD.equals(word)) {
             // double amt = Vendor.getTotalSales();
-            machine1.reload();
-            machine2.reload();
-            machine3.reload();
+            for (int i = 0; i < machines.length; i++) {
+                machines[i].reload();
+            }
+//            machine1.reload();
+//            machine2.reload();
+//            machine3.reload();
             JOptionPane.showMessageDialog(null,
                     // String.format("Total sales: $%.2f\n", amt) +
                     "Machines reloaded",
@@ -70,8 +91,16 @@ public class SnackBar extends JFrame
     // *****************************************************
 
     public static void main(String[] args) {
-        SnackBar window = new SnackBar();
-        window.setBounds(50, 50, 520, 310);
+        int m = 3;
+        if (args.length == 1) {
+            try {
+                m = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println("Not a valid number. Use default of 3");
+            }
+        }
+        SnackBar window = new SnackBar(m);
+        window.setBounds(50, 50, (520 / 3 * m), 310);
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setVisible(true);
